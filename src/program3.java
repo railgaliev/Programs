efd52152aa79584de60f8a172df907d0895fa597
railgaliev <rigaliev.net@gmail.com>
2021-08-09 14:10:17 +0300

    add program3

--- src/program3.java
+++ src/program3.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class program3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.print(b/a);
+        in.close();
+    }
 }
